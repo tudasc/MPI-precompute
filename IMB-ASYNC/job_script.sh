c6a2f4b91a8b5eba7711d21c319366eaafc7afdd
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-09-19 14:06:32 +0200

    job_script with more parameters

--- IMB-ASYNC/job_script.sh
+++ IMB-ASYNC/job_script.sh
@@ -8,16 +8,14 @@
 ###SBATCH --time 00:30:00
 # approx half a minute per benchmark, 4 benchmarks * 28 parameters
 # approx 2h
-#SBATCH --time 00:05:00
+#SBATCH --time 2:00:00
 
 #same as -c
 #SBATCH --cpus-per-task 96
 # one node per process
 
 ###SBATCH --array 0-1
-###SBATCH --array 1-20
-# 1 run for each parameter
-#SBATCH --array 1-240
+#SBATCH --array 1-100
 ###SBATCH --array 0-10
 
 #same as -j
@@ -27,11 +25,18 @@
 ## The real output will be saved into yml files
 ####SBATCH --output /dev/null
 
-# config
-OUTPATH=/work/scratch/tj75qeje/mpi-comp-match/output/measurement_$SLURM_NPROCS
-NUM_PARAMS=12
 
-TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
+
+# config
+OUTPATH=/work/scratch/tj75qeje/mpi-comp-match/output/$SLURM_NPROCS
+MODE=normal
+# 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+MOD=0
+
+
+## CYCLES=64
+
+TIMEOUT_CMD="/usr/bin/timeout -k 60 60"
 #here the jobscript starts
 #srun hostname
 
@@ -40,6 +45,16 @@ ml gcc/8.3.1
 ml hwloc/2.5.0 clang/11.1.0
 ml openucx/1.12.0
 
+if [[ "$MO" -eq 0 ]]; then
+ml openmpi/normal
+elif [[ "$MOD" -eq 1 ]]; then
+ml openmpi/rendevouz1
+elif [[ "$MOD" -eq 2 ]]; then
+ml openmpi/rendevouz2
+else
+ml openmpi/eager
+fi
+
 #export OMPI_MCA_opal_warn_on_missing_libcuda=0
 #export OMPI_MCA_opal_common_ucx_opal_mem_hooks=1
 export OMPI_MCA_osc=ucx
@@ -47,36 +62,41 @@ export OMPI_MCA_pml=ucx
 export UCX_WARN_UNUSED_ENV_VARS=n
 export UCX_UNIFIED_MODE=y
 
+I=0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tj75qeje/mpi-comp-match/IMB-ASYNC/src_cpp/ASYNC/thirdparty/lib/
 
 mkdir -p $OUTPATH
 
-MOD=$(( SLURM_ARRAY_TASK_ID % 4 ))
-MOD_PARAM=$(( ((SLURM_ARRAY_TASK_ID / 4 ) % NUM_PARAMS ) + 1 ))
+# random parameterorder, to add random disturbance (e.g. the slurm controler will interrupt quite regularly)
+RAND_PARAMS=$(shuf /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt)
+# change delimiter (IFS) to new line.
+IFS_BAK=$IFS
+IFS=$'\n'
+for PARAM in $RAND_PARAMS; do
 
+# return delimiter to previous value
+IFS=$IFS_BAK
+IFS_BAK=
 
-PARAM=$(sed "${MOD_PARAM}q;d" /home/tj75qeje/mpi-comp-match/IMB-ASYNC/parameters.txt)
+((I=I+1))
+
 # get calctime out of param, as calctime will not be included inside the yml, we need to include it in the filename
 CALCTIME=$(echo $PARAM | cut -d' ' -f2)
 
-if [[ "$MOD" -eq 0 ]]; then
-ml openmpi/normal
+for CYCLES in $(seq 2 2 64); do
 
-$TIMEOUT_CMD srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/normal_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
-elif [[ "$MOD" -eq 1 ]]; then
+$TIMEOUT_CMD srun ./IMB-ASYNC_orig async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles $CYCLES -nwarmup 0 $PARAM -output $OUTPATH/${MODE}_calctime_${CALCTIME}_cycles_${CYCLES}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
 
-ml openmpi/rendevouz1
+done # done for CYCLEs
 
-$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/rendevouz1_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
-elif [[ "$MOD" -eq 2 ]]; then
-ml openmpi/rendevouz2
-
-$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/rendevouz2_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
-else
-ml openmpi/eager
-
-$TIMEOUT_CMD srun ./IMB-ASYNC async_persistentpt2pt -cper10usec 64 -workload calc -thread_level single -datatype char -ncycles 64 -nwarmup 10 $PARAM -output $OUTPATH/eager_calctime_${CALCTIME}.$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID.$I.yaml >& /dev/null
-fi
+# change delimiter (IFS) to new line.
+IFS_BAK=$IFS
+IFS=$'\n'
+done 
+# return delimiter to previous value
+IFS=$IFS_BAK
+IFS_BAK=
 
 
+echo "done"
 
