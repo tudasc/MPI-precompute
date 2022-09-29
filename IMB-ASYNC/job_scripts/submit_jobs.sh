3b0e1ed3a734dfa56e5f858b5eccf114d7c09261
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-09-29 21:04:21 +0200

    new version of runner scripts with lots of smaller jobs

---
+++ IMB-ASYNC/job_scripts/submit_jobs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#small helper, that submits all 4 job arrays one for each communication mode
+
+#N_PARAM="-n 384 --tasks-per-node 96"
+#N_PARAM="-n 192 --tasks-per-node 96"
+N_PARAM="-n 2 --tasks-per-node 1 --exclusive"
+export PARAM_FILE="/home/tj75qeje/mpi-comp-match/IMB-ASYNC/job_scripts/parameters.txt"
+
+
+
+ARRAY_SIZE=$(wc -l $PARAM_FILE | cut -d' ' -f1)
+
+export MODE=normal
+# 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+export MOD=0
+sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+
+#export MODE=rendevouz1
+## 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+#export MOD=1
+#sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+
+export MODE=rendevouz2
+# 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+export MOD=2
+sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+
+#export MODE=eager
+## 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+#export MOD=3
+#sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+
+
+
