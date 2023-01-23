08bbaeec5d8cfcfd428a5556b01f7a23e7221c5c
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-01-23 12:58:52 +0100

    added comment about remaining warning

--- openmpi-patch/mpi-internals.h
+++ openmpi-patch/mpi-internals.h
@@ -5,7 +5,9 @@
 // some dont have an include guard, therefore we bundle them up all together
 // here
 
+//TODO get rid of the implicit function definition of asprintf warning
 #define _GNU_SOURCE
+#define __STDC_WANT_LIB_EXT2 1
 #include <stdio.h>
 
 #include "ompi/mca/osc/base/base.h"
