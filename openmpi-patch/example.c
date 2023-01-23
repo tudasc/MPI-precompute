c46b89cc5c9c3f1f564deafa94c9e1a033b2fa42
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-01-23 13:20:55 +0100

    properly initialize mem

--- openmpi-patch/example.c
+++ openmpi-patch/example.c
@@ -148,7 +148,7 @@ void use_standard_comm() {
   // wie viele Tasks gibt es?
   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
   int *buffer = malloc(N * sizeof(int));
-  double *work_buffer = malloc(N * sizeof(double));
+  double *work_buffer = calloc(N, sizeof(double));
   work_buffer[N - 1] = 0.6;
 
   MPI_Request req;
@@ -196,7 +196,7 @@ void use_persistent_comm() {
   // wie viele Tasks gibt es?
   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
   int *buffer = malloc(N * sizeof(int));
-  double *work_buffer = malloc(N * sizeof(double));
+  double *work_buffer = calloc(N, sizeof(double));
   work_buffer[N - 1] = 0.6;
 
   MPI_Request req;
