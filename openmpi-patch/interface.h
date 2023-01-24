53464ab4433c36aa17ab5254f1e8f42139eb578b
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-01-24 13:48:41 +0100

    format code

--- openmpi-patch/interface.h
+++ openmpi-patch/interface.h
@@ -11,24 +11,28 @@
 OMPI_DECLSPEC int MPIOPT_Start(MPI_Request *request);
 OMPI_DECLSPEC int MPIOPT_Wait(MPI_Request *request, MPI_Status *status);
 OMPI_DECLSPEC int MPIOPT_Waitall(int count, MPI_Request array_of_requests[],
-                   MPI_Status array_of_statuses[]);
-OMPI_DECLSPEC int MPIOPT_Waitany(int count, MPI_Request array_of_requests[], int *index,
-                   MPI_Status *status);
-OMPI_DECLSPEC int MPIOPT_Waitsome(int incount, MPI_Request array_of_requests[], int *outcount,
-                    int array_of_indices[], MPI_Status array_of_statuses[]);
-OMPI_DECLSPEC int MPIOPT_Test(MPI_Request *request, int *flag, MPI_Status *status);
-OMPI_DECLSPEC int MPIOPT_Testany(int count, MPI_Request array_of_requests[], int *index,
-                   int *flag, MPI_Status *status);
-OMPI_DECLSPEC int MPIOPT_Testall(int count, MPI_Request array_of_requests[], int *flag,
-                   MPI_Status array_of_statuses[]);
-OMPI_DECLSPEC int MPIOPT_Testsome(int incount, MPI_Request array_of_requests[], int *outcount,
-                    int array_of_indices[], MPI_Status array_of_statuses[]);
-OMPI_DECLSPEC int MPIOPT_Send_init(const void *buf, int count, MPI_Datatype datatype,
-                     int dest, int tag, MPI_Comm comm, MPI_Request *request);
-OMPI_DECLSPEC int MPIOPT_Recv_init(void *buf, int count, MPI_Datatype datatype, int source,
-                     int tag, MPI_Comm comm, MPI_Request *request);
+                                 MPI_Status array_of_statuses[]);
+OMPI_DECLSPEC int MPIOPT_Waitany(int count, MPI_Request array_of_requests[],
+                                 int *index, MPI_Status *status);
+OMPI_DECLSPEC int MPIOPT_Waitsome(int incount, MPI_Request array_of_requests[],
+                                  int *outcount, int array_of_indices[],
+                                  MPI_Status array_of_statuses[]);
+OMPI_DECLSPEC int MPIOPT_Test(MPI_Request *request, int *flag,
+                              MPI_Status *status);
+OMPI_DECLSPEC int MPIOPT_Testany(int count, MPI_Request array_of_requests[],
+                                 int *index, int *flag, MPI_Status *status);
+OMPI_DECLSPEC int MPIOPT_Testall(int count, MPI_Request array_of_requests[],
+                                 int *flag, MPI_Status array_of_statuses[]);
+OMPI_DECLSPEC int MPIOPT_Testsome(int incount, MPI_Request array_of_requests[],
+                                  int *outcount, int array_of_indices[],
+                                  MPI_Status array_of_statuses[]);
+OMPI_DECLSPEC int MPIOPT_Send_init(const void *buf, int count,
+                                   MPI_Datatype datatype, int dest, int tag,
+                                   MPI_Comm comm, MPI_Request *request);
+OMPI_DECLSPEC int MPIOPT_Recv_init(void *buf, int count, MPI_Datatype datatype,
+                                   int source, int tag, MPI_Comm comm,
+                                   MPI_Request *request);
 OMPI_DECLSPEC int MPIOPT_Request_free(MPI_Request *request);
-
 
 OMPI_DECLSPEC void MPIOPT_INIT();
 OMPI_DECLSPEC void MPIOPT_FINALIZE();
