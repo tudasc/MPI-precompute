dbe213f82f80325ba0d094cd137fbd86fc667abc
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-02-15 14:01:31 +0100

    Most recent version of scripts

--- IMB-ASYNC/job_scripts/submit_jobs.sh
+++ IMB-ASYNC/job_scripts/submit_jobs.sh
@@ -4,6 +4,8 @@
 
 #N_PARAM="-n 384 --tasks-per-node 96"
 #N_PARAM="-n 192 --tasks-per-node 96"
+
+#N_PARAM="-n 64 --tasks-per-node 32 --exclusive"
 N_PARAM="-n 2 --tasks-per-node 1 --exclusive"
 export PARAM_FILE="/home/tj75qeje/mpi-comp-match/IMB-ASYNC/job_scripts/parameters.txt"
 
@@ -16,20 +18,20 @@ export MODE=normal
 export MOD=0
 sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
 
-#export MODE=rendevouz1
-## 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
-#export MOD=1
-#sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+export MODE=rendevouz1
+# 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+export MOD=1
+sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
 
 export MODE=rendevouz2
 # 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
 export MOD=2
 sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
 
-#export MODE=eager
-## 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
-#export MOD=3
-#sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
+export MODE=eager
+# 0,1,2,3 for normal,rendevouz1,rendevouz2,eager
+export MOD=3
+sbatch $N_PARAM -A $CUR_PROJ --array 1-$ARRAY_SIZE job_script.sh
 
 
 
