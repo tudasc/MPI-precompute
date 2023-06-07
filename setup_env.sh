f4887c6e2b863c8bee68b9986d267fdcc26d5110
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-06-07 16:01:32 +0200

    use the env vars

--- setup_env.sh
+++ setup_env.sh
@@ -12,9 +12,9 @@ ml openmpi/4.1.1
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tj75qeje/mpi-comp-match/IMB-ASYNC/src_cpp/ASYNC/thirdparty/lib/
 
-#export OMPI_MCA_opal_warn_on_missing_libcuda=0
-#export OMPI_MCA_opal_common_ucx_opal_mem_hooks=1
-#export OMPI_MCA_osc=ucx
-#export OMPI_MCA_pml=ucx
-#export UCX_WARN_UNUSED_ENV_VARS=n
-#export UCX_UNIFIED_MODE=y
+export OMPI_MCA_opal_warn_on_missing_libcuda=0
+export OMPI_MCA_opal_common_ucx_opal_mem_hooks=1
+export OMPI_MCA_osc=ucx
+export OMPI_MCA_pml=ucx
+export UCX_WARN_UNUSED_ENV_VARS=n
+export UCX_UNIFIED_MODE=y
