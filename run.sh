4b8dcca0b9510fc7e6188009e8c92f06d3f8720e
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-07-12 15:42:14 +0200

    path of Pass as env var

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#using openmpi:
-
-#INCLUDE="-I/home/td37cari/openmpi-4.1.1/ -I/home/td37cari/openmpi-4.1.1/opal/include -I/home/td37cari/openmpi-4.1.1/ompi/include/ -I/home/td37cari/openmpi-4.1.1/orte/include"
-INCLUDE=""
-
 #CFLAGS="-std=c11 -O3 ${INCLUDE}"
 CFLAGS="-std=c11 -O0 -g ${INCLUDE}"
 LIBS="-lopen-pal -lucp -lm"
@@ -14,13 +9,12 @@ export MPI_COMPILER_ASSISTANCE_FRONTEND_PLUGIN_FILE="plugin_data.json"
 #
 if [ ${1: -2} == ".c" ]; then
 
-OMPI_CC=clang $MPICC $CFLAGS -fpass-plugin=build/mpi_compiler_assistance_matching_pass/libmpi_compiler_assistance_matching_pass.so  $1 $LIBS
-#OMPI_CC=clang $MPICC $CFLAGS $1 $LIBS
-#$MPICC -cc=clang -O2 -fopenmp -Xclang -load -Xclang build/mpi_compiler_assistance_matching_pass/mpi_compiler_assistance_matching_pass.so  -ftime-report $1
+OMPI_CC=clang $MPICC $CFLAGS -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS $1 $LIBS
 OMPI_CC=clang $MPICC $CFLAGS -o a.out_original $1 $LIBS
 
 elif [ ${1: -4} == ".cpp" ]; then
-OMPI_CXX=clang++ $MPICXX $CFLAGS -fpass-plugin=build/mpi_compiler_assistance_matching_pass/libmpi_compiler_assistance_matching_pass.so  $1 $LIBS
+OMPI_CXX=clang++ $MPICXX $CFLAGS -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS  $1 $LIBS
+OMPI_CXX=clang $MPICC $CXXFLAGS -o a.out_original $1 $LIBS
 else
 echo "Unknown file suffix, use this script with .c or .cpp files"
 fi
