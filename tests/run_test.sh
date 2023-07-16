a0414317bab92d68b160f8a6f11786dcb6db798e
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-07-16 19:28:21 +0200

    also added check that compile time analysis workes as expected

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -18,6 +18,23 @@ cd $TEST_DIR
 
 $RUN_SCRIPT *.c
 
+#check if compiler analysis cas successfull
+
+EXPECT_MPIOPT=1
+
+objdump -T ./a.out | grep -q MPIOPT
+NO_MPI_OPT= $?
+# 1 if grep was empty, 0 if functions starting with MPIOPT are used
+
+if [[ "EXPECT_MPIOPT" == 1 && "$NO_MPI_OPT" == 1 ]]; then
+  echo "Expected MPIOPT usage but none found"
+  exit 1
+fi
+if [[ "EXPECT_MPIOPT" == 0 && "$NO_MPI_OPT" == 0 ]]; then
+  echo "Expected NOP MPIOPT usage but found usage"
+  exit 1
+fi
+
 mpirun -n 2 ./a.out_original
 mpirun -n 2 ./a.out
 
