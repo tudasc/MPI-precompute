e0416e623616f6e13602f9b0d4c70c824a1535d8
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-08-14 20:50:50 +0200

    added WholeProgramDevirt Pass form LLVM

--- run.sh
+++ run.sh
@@ -5,16 +5,18 @@ CFLAGS="-std=c11 -O1 -g ${INCLUDE}"
 LIBS="-lopen-pal -lucp -lm"
 CXXFLAGS="-std=c++17 -O1 -g ${INCLUDE}"
 
+PASS_FLAGS="-fwhole-program-vtables -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS"
+
 
 export MPI_COMPILER_ASSISTANCE_FRONTEND_PLUGIN_FILE="plugin_data.json"
 #
 if [ ${1: -2} == ".c" ]; then
 
-OMPI_CC=clang $MPICC $CFLAGS -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS $1 $LIBS
+OMPI_CC=clang $MPICC $CFLAGS $PASS_FLAGS $1 $LIBS
 OMPI_CC=clang $MPICC $CFLAGS -o a.out_original $1 $LIBS
 
 elif [ ${1: -4} == ".cpp" ]; then
-OMPI_CXX=clang++ $MPICXX $CXXFLAGS -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS  $1 $LIBS
+OMPI_CXX=clang++ $MPICXX $CXXFLAGS $PASS_FLAGS $1 $LIBS
 OMPI_CXX=clang++ $MPICXX $CXXFLAGS -o a.out_original $1 $LIBS
 else
 echo "Unknown file suffix, use this script with .c or .cpp files"
