d4a3d40d0e17474af0b3525229c06d192b95a644
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-10-24 14:39:25 +0200

    Refactoring WIP: usage of funcs from precompute library

---
+++ mpi_compiler_assistance_matching_pass/precompute_funcs.cpp
@@ -0,0 +1,54 @@
+/*
+  Copyright 2020 Tim Jammer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#include "precompute_funcs.h"
+
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Module.h"
+
+#include "llvm/Support/raw_ostream.h"
+#include <mpi.h>
+
+using namespace llvm;
+
+PrecomputeFunctions *PrecomputeFunctions::instance = nullptr;
+
+PrecomputeFunctions::PrecomputeFunctions(Module &M) {
+
+  auto *ftype = FunctionType::get(
+      Type::getVoidTy(M.getContext()),
+      {Type::getInt32Ty(M.getContext()), Type::getInt32Ty(M.getContext())},
+      false);
+
+  register_precomputed_value = cast<Function>(
+      M.getOrInsertFunction("register_precomputed_value", ftype, {})
+          .getCallee()
+          ->stripPointerCasts());
+
+  ftype = FunctionType::get(Type::getVoidTy(M.getContext()),
+                            {Type::getVoidTy(M.getContext())}, false);
+
+  init_precompute_lib =
+      cast<Function>(M.getOrInsertFunction("init_precompute_lib", ftype, {})
+                         .getCallee()
+                         ->stripPointerCasts());
+
+  finish_precomputation =
+      cast<Function>(M.getOrInsertFunction("finish_precomputation", ftype, {})
+                         .getCallee()
+                         ->stripPointerCasts());
+}
---
+++ mpi_compiler_assistance_matching_pass/precompute_funcs.h
@@ -0,0 +1,49 @@
+
+#ifndef MACH_PRECOMPUTE_FUNCS_H_
+#define MACH_PRECOMPUTE_FUNCS_H_
+
+#include "llvm/IR/Constant.h"
+#include "llvm/IR/Module.h"
+#include <cassert>
+
+// holds the llvm::Function* for the functions from the precompute library
+class PrecomputeFunctions {
+
+public:
+  // singelton-like
+  static PrecomputeFunctions *get_instance() {
+    assert(instance != nullptr);
+    return instance;
+  }
+
+  static PrecomputeFunctions *create_instance(llvm::Module &M) {
+    if (instance == nullptr)
+      instance = new PrecomputeFunctions(M);
+    return instance;
+  }
+
+  static void delete_instance() {
+    if (instance != nullptr)
+      delete instance;
+    instance = nullptr;
+  }
+
+  // singelton pattern: singeltons are NOT copy-able or assignable
+  PrecomputeFunctions(PrecomputeFunctions &other) = delete;
+
+  void operator=(const PrecomputeFunctions &) = delete;
+
+private:
+  static PrecomputeFunctions *instance;
+
+  PrecomputeFunctions(llvm::Module &M);
+
+  ~PrecomputeFunctions();
+
+public:
+  llvm::Function *init_precompute_lib;
+  llvm::Function *finish_precomputation;
+  llvm::Function *register_precomputed_value;
+};
+
+#endif /* MACH_PRECOMPUTE_FUNCS_H_ */
