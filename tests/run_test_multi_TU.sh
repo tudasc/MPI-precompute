0bf5a317741488a2761aed6354a91e284b4d8705
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-10-27 12:48:30 +0200

    Test infrastructure for multi TU tests with cmake

--- tests/run_test_multi_TU.sh
+++ tests/run_test_multi_TU.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-echo "$(pwd)"
 
-TEST_DIR=$1
+echo $(pwd)
+# where the wrappers are found
+BINARY_DIR=$1
 
-source ./setup_env.sh
+# the testcase to use
+TEST_DIR=$2
+
+source ${BINARY_DIR}/setup_env.sh
 
 if [ ! -d "$TEST_DIR" ]; then
   echo "$TEST_DIR is not a directory"
   exit 1
 fi
 
+cd $TEST_DIR
+mkdir build
+cd build
+
+# without pass enabled to get the original application
+export USE_MPI_COMPILER_ASSISTANCE_PASS=false
 export OMPI_CC=clang
 export OMPI_CXX=clang++
 
-cd $TEST_DIR
-
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_C_FLAGS="-flto -fuse-ld=lld -fwhole-program-vtables" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="-flto -fuse-ld=lld -fwhole-program-vtables" $TEST_DIR
 make
 
-mpirun -n 2 ./a.out | grep "Usage of MPIOPT optimized communication sceme"
+mpirun -n 2 ./test | grep "Usage of MPIOPT optimized communication sceme"
 SAVE_STATUS=( "${PIPESTATUS[@]}" )
 MPIRUN_STATUS=${SAVE_STATUS[0]}
 NO_MPI_OPT=${SAVE_STATUS[1]}
-
-  make clean
 
 if [[ "$MPIRUN_STATUS" != 0 ]]; then
   echo "Crash of ORIGINAL result application - testcase probably broken"
   exit 1
 fi
-
 
 # 1 if grep was empty, 0 otherwise
 if [[ "$NO_MPI_OPT" != 1 ]]; then
@@ -37,19 +43,24 @@ if [[ "$NO_MPI_OPT" != 1 ]]; then
   exit 1
 fi
 
-# rebuild with our compiler pass enabled
-export LFLAGS="-Og -g -fpass-plugin=$MPI_COMPILER_ASSISTANCE_PASS -lprecompute"
+# rebuild with pass enabled
+# also remove all cmake file to start build process from scratch
+rm -rf *
+
+export USE_MPI_COMPILER_ASSISTANCE_PASS=true
+export OMPI_CC="${BINARY_DIR}/clang_wrap_cc"
+export OMPI_CXX="${BINARY_DIR}/clang_wrap_cxx"
+
+cmake -DCMAKE_C_COMPILER="${BINARY_DIR}/clang_wrap_cc" -DCMAKE_C_FLAGS="-flto -fuse-ld=lld -fwhole-program-vtables" -DCMAKE_CXX_COMPILER="${BINARY_DIR}/clang_wrap_cxx" -DCMAKE_CXX_FLAGS="-flto -fuse-ld=lld -fwhole-program-vtables" $TEST_DIR
+
 make
-#check if compiler analysis was successfull
+
 EXPECT_MPIOPT=1
 
-
-mpirun -n 2 ./a.out | grep "Usage of MPIOPT optimized communication sceme"
+mpirun -n 2 ./test | grep "Usage of MPIOPT optimized communication sceme"
 SAVE_STATUS=( "${PIPESTATUS[@]}" )
 MPIRUN_STATUS=${SAVE_STATUS[0]}
 NO_MPI_OPT=${SAVE_STATUS[1]}
-
-  make clean
 
 if [[ "$MPIRUN_STATUS" != 0 ]]; then
   echo "Crash of result application"
@@ -64,4 +75,5 @@ if [[ "$EXPECT_MPIOPT" == 0 && "$NO_MPI_OPT" != 1 ]]; then
   echo "Expected NO MPIOPT usage but found usage"
   exit 1
 fi
+
 
