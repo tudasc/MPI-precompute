0bad89daa9178c2cfd62d8580ae425bbab1e625c
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-03-26 13:37:44 +0100

    add user-exposed openmp funcs as openmp funcs

--- precomputation_compiler_lib/openmp_runtime_functions.h
+++ precomputation_compiler_lib/openmp_runtime_functions.h
@@ -16,7 +16,8 @@ struct omp_functions *get_omp_functions(llvm::Module &M);
 
 inline bool is_omp_function(llvm::Function *func) {
   auto omp_funcs = get_omp_functions(*func->getParent());
-  return func->getName().starts_with("__kmpc_");
+  return func->getName().starts_with("__kmpc_") ||
+         func->getName().starts_with("omp_");
 
   return func == omp_funcs->kmpc_fork_call ||
          func == omp_funcs->kmpc_global_thread_num ||
