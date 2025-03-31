972d1b566df786b8a478eb42f33ae68cb900a392
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-03-31 14:15:58 +0200

    remove unused code

--- precomputation_compiler_lib/openmp_runtime_functions.h
+++ precomputation_compiler_lib/openmp_runtime_functions.h
@@ -18,14 +18,9 @@ struct omp_functions {
 
 struct omp_functions *get_omp_functions(llvm::Module &M);
 
-inline bool is_omp_function(llvm::Function *func) {
-  auto omp_funcs = get_omp_functions(*func->getParent());
+inline bool is_omp_function(const llvm::Function *func) {
   return func->getName().starts_with("__kmpc_") ||
          func->getName().starts_with("omp_");
-
-  return func == omp_funcs->kmpc_fork_call ||
-         func == omp_funcs->kmpc_global_thread_num ||
-         func == omp_funcs->kmpc_push_num_threads;
 }
 
 inline bool is_omp_fork_call(llvm::CallBase *call) {
