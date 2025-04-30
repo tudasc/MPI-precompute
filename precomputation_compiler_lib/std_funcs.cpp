7868d878827c9a4ddfcabfb96ea6537e49fafe73
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-04-30 15:31:32 +0200

    add another std func

--- precomputation_compiler_lib/std_funcs.cpp
+++ precomputation_compiler_lib/std_funcs.cpp
@@ -174,7 +174,7 @@ bool is_func_from_std(llvm::Function *func) {
       // as one should usa a random seed anyway it doesn't matter if we call
       // it in precompute
       func->getName() == "getrusage" || func->getName() == "time" ||
-      func->getName() == "localtime" ||
+      func->getName() == "localtime" || func->getName() == "clock_gettime" ||
       // from gnu
       func->getName() == "__getdelim") {
 
