64efee1fd58eb57dd115a256262f7932b22cc89e
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-05-05 16:13:29 +0200

    more std functions

--- precomputation_compiler_lib/std_funcs.cpp
+++ precomputation_compiler_lib/std_funcs.cpp
@@ -179,7 +179,8 @@ bool is_func_from_std(llvm::Function *func) {
       // it in precompute
       func->getName() == "getrusage" || func->getName() == "time" ||
       func->getName() == "localtime" || func->getName() == "clock_gettime" ||
-      func->getName() == "isspace" ||
+      func->getName() == "isspace" || func->getName() == "isalpha" ||
+      func->getName() == "isalnum" || func->getName() == "isdigit" ||
 
       // from gnu
       func->getName() == "__getdelim") {
