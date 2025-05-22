335f1e364baaffe795acf5b5f2cc58bf98cb5dae
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-05-22 12:40:37 +0200

    added std func

--- precomputation_compiler_lib/std_funcs.cpp
+++ precomputation_compiler_lib/std_funcs.cpp
@@ -179,8 +179,9 @@ bool is_func_from_std(llvm::Function *func) {
       // it in precompute
       func->getName() == "getrusage" || func->getName() == "time" ||
       func->getName() == "localtime" || func->getName() == "clock_gettime" ||
-      func->getName() == "isspace" || func->getName() == "isalpha" ||
-      func->getName() == "isalnum" || func->getName() == "isdigit" ||
+      func->getName() == "strftime" || func->getName() == "isspace" ||
+      func->getName() == "isalpha" || func->getName() == "isalnum" ||
+      func->getName() == "isdigit" ||
 
       // from gnu
       func->getName() == "__getdelim") {
