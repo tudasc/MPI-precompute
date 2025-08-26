3b3e17b6c1eaed806982ea1075c041c02abcd317
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-08-26 14:42:56 +0200

    fix link flags to support test

--- use-cases/thread-sanitizer/tests/fortran/test_fortran.sh
+++ use-cases/thread-sanitizer/tests/fortran/test_fortran.sh
@@ -14,7 +14,8 @@ GREP_STRING="WARNING: ThreadSanitizer: data race"
 
 rm ./a.out
 
-COMPILER_FLAGS="-O2 -g -fopenmp -flto -fuse-ld=lld -ltsan"
+# manually link the required tsan libraries as flang currently dies not support -fsanitize=thread
+COMPILER_FLAGS="-O2 -g -fopenmp -flto -fuse-ld=lld -Wl,--whole-archive /home/tim/llvm_20/lib/clang/21/lib/x86_64-pc-linux-gnu/libclang_rt.tsan.a -Wl,--no-whole-archive -Wl,--dynamic-list=/home/tim/llvm_20/lib/clang/21/lib/x86_64-pc-linux-gnu/libclang_rt.tsan.a.syms -ldl"
 
 # compile
 # with pass
